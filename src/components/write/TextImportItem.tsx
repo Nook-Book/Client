9226d17daf54b399c54b9c9748cb9fa435e9e632
rlotr02 <0314kelee@naver.com>
2024-08-05 19:49:18 +0900

    feat: 텍스트 추출 UI 제작

--- src/components/write/TextImportItem.tsx
+++ src/components/write/TextImportItem.tsx
@@ -1,5 +1,8 @@
-import { View, Text, FlatList, Pressable } from "react-native";
+import { View, Text, FlatList, Pressable, Alert } from "react-native";
 import { styles } from "../../styles/write/ImageItemStyle";
+import * as ImagePicker from "expo-image-picker";
+import { useEffect, useState } from "react";
+import { Camera } from "expo-camera";
 
 type TextImportItemType = {
   text: string;
@@ -11,8 +14,68 @@ const TextImportItem = ({
 }: {
   handleTextInsert: (type: string) => void;
 }) => {
-  const openCamera = () => {};
-  const openAlbum = () => {};
+  const [hasCameraPermission, setHasCameraPermission] = useState<
+    boolean | null
+  >(null);
+  const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState<
+    boolean | null
+  >(null);
+
+  //카메라, 앨범 권한 요청
+  useEffect(() => {
+    (async () => {
+      const { status: cameraStatus } =
+        await Camera.requestCameraPermissionsAsync();
+      const { status: mediaLibraryStatus } =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      setHasCameraPermission(cameraStatus === "granted");
+      setHasMediaLibraryPermission(mediaLibraryStatus === "granted");
+    })();
+  }, []);
+
+  //앨범에서 이미지 선택
+  const openAlbum = async () => {
+    if (!hasMediaLibraryPermission) {
+      Alert.alert("권한 오류", "미디어 라이브러리 접근 권한이 필요합니다.");
+      return;
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      //텍스트 추출로 수정 필요
+      const imageUri = result.assets[0].uri;
+      const markdownImageSyntax = `![Image](${imageUri})\n`;
+      handleTextInsert(markdownImageSyntax);
+    }
+  };
+
+  //카메라 실행
+  const openCamera = async () => {
+    if (!hasCameraPermission) {
+      Alert.alert("권한 오류", "카메라 접근 권한이 필요합니다.");
+      return;
+    }
+
+    const result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      //텍스트 추출로 수정 필요
+      const imageUri = result.assets[0].uri;
+      const markdownImageSyntax = `![Image](${imageUri})\n`;
+      handleTextInsert(markdownImageSyntax);
+    }
+  };
 
   const TextImportItem: TextImportItemType[] = [
     {
