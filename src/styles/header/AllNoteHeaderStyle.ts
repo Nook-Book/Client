0f47a6baa98e00d8b93008111839c47e0db6cf51
rlotr02 <0314kelee@naver.com>
2024-08-06 23:59:59 +0900

    feat: Back Icon 색상 추가

--- src/styles/header/AllNoteHeaderStyle.ts
+++ src/styles/header/AllNoteHeaderStyle.ts
@@ -13,5 +13,6 @@ export const styles = StyleSheet.create({
   button: {
     width: 24,
     height: 24,
+    color: Color.Contents.Icon,
   },
 });
--- src/styles/header/BackHeaderStyle.ts
+++ src/styles/header/BackHeaderStyle.ts
@@ -14,6 +14,7 @@ export const styles = StyleSheet.create({
     height: 24,
     position: "absolute",
     left: 20,
+    color: Color.Contents.Icon,
   },
   text1: {
     ...Font.Label.XMedium,
