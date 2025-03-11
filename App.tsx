2b0ca07f8089616be2c442b03f5c593c9ff5e76b
si14444 <si14444@naver.com>
2025-03-11 14:59:45 +0900

    feat: 카카오 로그인 연동

--- App.tsx
+++ App.tsx
@@ -3,28 +3,19 @@ import { useFonts } from "expo-font";
 import * as React from "react";
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import OnboardingOverlay from "./src/components/libary/OnboardingOverlay";
-import TabNavigation from "./src/components/navBar/TabNavigation";
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-import * as Linking from "expo-linking";
+import OnboardingOverlay from "./src/components/libary/OnboardingOverlay";
+import LoginStackScreen from "./src/components/navBar/LoginStackScreen";
+import TabNavigation from "./src/components/navBar/TabNavigation";
 
 export default function App() {
   const queryClient = new QueryClient();
 
-  const linking = {
-    prefixes: [Linking.createURL("/")],
-    config: {
-      screens: {
-        Detail: "book",
-      },
-    },
-  };
-
   //처음 방문 시 온보딩 화면 실행
   const [showOnboarding, setShowOnboarding] = React.useState(false);
 
+  //로그인 여부 확인 상태 (추후 수정할 것)
+  const [isLogin, setIsLogin] = React.useState(false);
   const handleOnboardingDismiss = () => {
     setShowOnboarding(false);
   };
@@ -46,16 +37,21 @@ export default function App() {
     SCDream6: require("./src/assets/fonts/SCDream6.otf"),
     SCDream5: require("./src/assets/fonts/SCDream5.otf"),
     SCDream4: require("./src/assets/fonts/SCDream4.otf"),
-    Oblique: require("./src/assets/fonts/Oblique.otf"),
   });
   if (!fontsLoaded) return null;
 
   return (
     <QueryClientProvider client={queryClient}>
-      <NavigationContainer linking={linking}>
-        <TabNavigation />
-        {showOnboarding && (
-          <OnboardingOverlay onDismiss={handleOnboardingDismiss} />
+      <NavigationContainer>
+        {!isLogin ? (
+          <LoginStackScreen />
+        ) : (
+          <>
+            <TabNavigation />
+            {showOnboarding && (
+              <OnboardingOverlay onDismiss={handleOnboardingDismiss} />
+            )}
+          </>
         )}
       </NavigationContainer>
     </QueryClientProvider>
