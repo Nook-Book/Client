b63865d94430839e59f47af7bca35f92acf23e34
si14444 <si14444@naver.com>
2025-05-18 17:50:35 +0900

    fix: url change

--- src/api/user-Mypage/getCollectionDetail.ts
+++ src/api/user-Mypage/getCollectionDetail.ts
@@ -2,6 +2,6 @@ import api from "..";
 
 // 컬렉션 상세 조회
 export const getCollectionDetail = async (collectionId: number) => {
-  const response = await api.get(`/api/v1/collection/${collectionId}/books`);
+  const response = await api.get(`/api/v1/collections/${collectionId}/books`);
   return response.data;
 };
