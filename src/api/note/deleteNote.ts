00e9d3ca414163dcc056a577329ac8ae96ec216c
rlotr02 <0314kelee@naver.com>
2025-06-03 15:35:27 +0900

    fix: 독서 노트 삭제 api 경로 수정

--- src/api/note/deleteNote.ts
+++ src/api/note/deleteNote.ts
@@ -3,7 +3,7 @@ import api from "..";
 //독서 노트 삭제
 export const deleteNote = async (noteId: number) => {
   try {
-    const response = await api.delete(`/api/v1/note/${noteId}`);
+    const response = await api.delete(`/api/v1/notes/${noteId}`);
     return response.data;
   } catch (e) {
     console.log(e);
