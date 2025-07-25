409573eb074f725ea8bd7533ef3a54d59cb89675
beomsu <beomsu144@gamil.com>
2025-07-26 01:27:36 +0900

    fix: 월별 독서 통계 조회 API 파라미터 수정 및 반환 타입 변경

--- src/api/user-Mypage/getReport.ts
+++ src/api/user-Mypage/getReport.ts
@@ -2,13 +2,14 @@ import api from "..";
 import useYear from "../../store/useYear";
 
 // 월별 독서 통계 조회
-export const getReport = async (): Promise<ResponseReport | undefined> => {
+export const getReport = async (): Promise<any | undefined> => {
   const { year } = useYear();
 
   try {
     const response = await api.get(`/api/v1/my-page/reports`, {
       params: {
-        year,
+        type: "year",
+        targetYear: year,
       },
     });
     return response.data;
