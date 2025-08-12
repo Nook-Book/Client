b131a15a239e9581138aa7ebbbf93bccf795dd1b
si14444 <si14444@naver.com>
2025-08-12 18:56:15 +0900

    fix: 베스트셀러 컴포넌트에서 책 목록을 3개씩 그룹화하고, 빈 슬롯을 null로 채워 레이아웃 유지 개선.

--- src/components/search/BookCollection.tsx
+++ src/components/search/BookCollection.tsx
@@ -8,6 +8,13 @@ import {
 } from "../../utils/category/bookCategories";
 import BestSellerBook from "./BestSellerBook";
 
+interface BookItem {
+  cover: string;
+  title: string;
+  author: string;
+  isbn13: string;
+}
+
 const BookCollection = ({
   isMarginBottom,
   category,
@@ -27,10 +34,17 @@ const BookCollection = ({
   const bestSellerList = data.item;
 
   // 3*4 배열을 만들기 위해 3개씩 푸시함.
-  const groupedBooks = [];
+  const groupedBooks: (BookItem | null)[][] = [];
+
   for (let i = 0; i < bestSellerList.length; i += 3) {
-    groupedBooks.push(bestSellerList.slice(i, i + 3));
+    const group: (BookItem | null)[] = bestSellerList.slice(i, i + 3);
+    // 마지막 그룹이 3개 미만이면 빈 슬롯으로 채움
+    while (group.length < 3) {
+      group.push(null);
+    }
+    groupedBooks.push(group);
   }
+
   return (
     <View style={isMarginBottom && { marginBottom: 90 }}>
       {groupedBooks.map((group, groupIndex) => (
@@ -43,16 +57,30 @@ const BookCollection = ({
               groupIndex === groupedBooks.length - 1 && { marginBottom: 16 },
           ]}
         >
-          {group.map((book, index) => (
-            <BestSellerBook
-              key={index}
-              id={groupIndex * group.length + index + 1}
-              image={book.cover}
-              title={book.title}
-              name={book.author}
-              isbn={book.isbn13}
-            />
-          ))}
+          {group.map((book, bookIndex) => {
+            const currentId = groupIndex * 3 + bookIndex + 1;
+
+            // null인 경우 빈 View 반환 (같은 width 유지)
+            if (!book) {
+              return (
+                <View
+                  key={`empty-${groupIndex}-${bookIndex}`}
+                  style={{ flex: 1, marginHorizontal: 9 }}
+                />
+              );
+            }
+
+            return (
+              <BestSellerBook
+                key={`${groupIndex}-${bookIndex}`}
+                id={currentId}
+                image={book.cover}
+                title={book.title}
+                name={book.author}
+                isbn={book.isbn13}
+              />
+            );
+          })}
         </View>
       ))}
     </View>
